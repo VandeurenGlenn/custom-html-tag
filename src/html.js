c563b7aea1f821be6ee40f95662b9437b393b666
vandeurenglenn <vandeurenglenn@gmail.com>
2018-06-07 00:18:50 +0200

    :neutral_face: Rendering happens in renderer
    Closes #2

--- src/html.js
+++ src/html.js
@@ -3,90 +3,17 @@
  * @example
  ```js
   const template = html`<h1>${'name'}</h1>`;
-  let templateResult = template({name: 'Olivia'})
-  element.innerHTML = templateResult.template;
-  templateResult = template({name: 'Jon'})
-  element.innerHTML = templateResult.template;
+  let templateResult = template({name: 'Olivia'});
 
-  // you can also update the changes only
-  templateResult.changes.forEach(change => {
-    change.from.value // previous value
-    change.from.position // previous position
-    change.to.value // new value
-    change.to.position // new position
-    // check https://github.com/vandeurenglenn/custom-renderer for an example how to implement.
-  });
-
+  templateResult.values // property values 'Olivia'
+  templateResult.keys // property keys 'name'
+  templateResult.strings // raw template array '["<h1>", "</h1>"]'
  ```
  */
-class HTMLTag {
-  constructor() {
-    this.set = []
-    this.html = this.html.bind(this)
-    return this.html;
-  }
-
-  html(strings, ...keys) {
-    const set = this.set
-    return ((...values) => {
-      const dict = values[values.length - 1] || {};
-      let template = strings[0];
-      let setChanged = false;
-      const changes = [];
-      if (values[0] !== undefined) {
-        keys.forEach((key, i) => {
-          let value = Number.isInteger(key) ? values[key] : dict[key];
-          if (value === undefined && Array.isArray(key)) {
-            value = key.join('');
-          } else if (value === undefined && !Array.isArray(key) && set[i]) {
-            value = set[i].value; // set previous value, doesn't require developer to pass all properties
-          } else if (value === undefined && !Array.isArray(key) && !set[i]) {
-            value = '';
-          }
-          const string = strings[i + 1];
-          const stringLength = string.length;
-          const start = template.length;
-          const end = template.length + value.length;
-          const position = [start, end];
-
-          if (set[i] && set[i].value !== value) {
-            setChanged = true;
-            changes.push({
-              from: {
-                value: set[i].value,
-                position: set[i].position,
-              },
-              to: {
-                value,
-                position
-              }
-            });
-            set[i].value = value;
-            set[i].position = [start, end];
-          } else if (!set[i]) {
-            set.push({value, position: [start, end]});
-            changes.push({
-              from: {
-                value: null,
-                position
-              },
-              to: {
-                value,
-                position
-              }
-            });
-          }
-          template += `${value}${string}`;
-        });
-      } else {
-        template += strings[0];
-      }
-      return {
-        template,
-        changes
-      };
-    });
-  }
+const html = (strings, ...keys) => {
+  return ((...values) => {
+    return {strings, keys, values};
+  });
 }
 
-export default HTMLTag;
+export default html;
